af029afb0dd69f53fae0aa979ac0cbb0e743efa5
sominJoo <js06m13@gmail.com>
2024-05-23 16:24:20 +0900

    django 프로젝트 셋팅 및 예시 API 추가

---
+++ llm-service/llm/apps.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class LLMConfig(AppConfig):
+    name = 'llm'
+    verbose_name = 'LLM Prototype Application'
--- llm-service/llmapp/urls.py
+++ llm-service/llmapp/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import  path
+from django.urls import include, path
 
 from llm.views import LlmAPIView
 
+api_urls = [
+    path('llm/', include('llm.urls')),
+]
+
 urlpatterns = [
-    path('', LlmAPIView.as_view()),
+    path('api/', include(api_urls)),
 ]
