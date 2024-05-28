7feff465137d80f95913fb618f29c1050f84043f
leejeongkang <krj1226@gmail.com>
2024-05-28 16:02:21 +0900

    refactor: optional -> composition 코드 수정

--- chat-ui/src/views/ChatView.vue
+++ chat-ui/src/views/ChatView.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <vue-advanced-chat
-        height="calc(100vh - 20px)"
-        :current-user-id="currentUserId"
+        :current-user-id="'USER'"
         :rooms="JSON.stringify(rooms)"
         :rooms-loaded="true"
+        :show-audio="false"
         :messages="JSON.stringify(messages)"
         :messages-loaded="messagesLoaded"
         @send-message="sendMessage($event.detail[0])"
@@ -13,88 +13,78 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { ref } from 'vue'
 import { register } from 'vue-advanced-chat'
-// import { register } from '../../vue-advanced-chat/dist/vue-advanced-chat.es.js'
-register()
+register();
 
-export default {
-  data() {
-    return {
-      currentUserId: '1234',
-      rooms: [
-        {
-          roomId: '1',
-          roomName: 'Room 1',
-          avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
-          users: [
-            { _id: '1234', username: 'John Doe' },
-            { _id: '4321', username: 'John Snow' }
-          ]
-        }
-      ],
-      messages: [],
-      messagesLoaded: false
-    }
-  },
-
-  methods: {
-    fetchMessages({ options = {} }) {
-      setTimeout(() => {
-        if (options.reset) {
-          this.messages = this.addMessages(true)
-        } else {
-          this.messages = [...this.addMessages(), ...this.messages]
-          this.messagesLoaded = true
-        }
-        // this.addNewMessage()
-      })
-    },
-
-    addMessages(reset) {
-      const messages = []
-
-      for (let i = 0; i < 30; i++) {
-        messages.push({
-          _id: reset ? i : this.messages.length + i,
-          content: `${reset ? '' : 'paginated'} message ${i + 1}`,
-          senderId: '4321',
-          username: 'John Doe',
-          date: '13 November',
-          timestamp: '10:20'
-        })
-      }
-
-      return messages
-    },
-
-    sendMessage(message) {
-      this.messages = [
-        ...this.messages,
-        {
-          _id: this.messages.length,
-          content: message.content,
-          senderId: this.currentUserId,
-          timestamp: new Date().toString().substring(16, 21),
-          date: new Date().toDateString()
-        }
-      ]
-    },
-
-    addNewMessage() {
-      setTimeout(() => {
-        this.messages = [
-          ...this.messages,
-          {
-            _id: this.messages.length,
-            content: 'NEW MESSAGE',
-            senderId: '1234',
-            timestamp: new Date().toString().substring(16, 21),
-            date: new Date().toDateString()
-          }
-        ]
-      }, 2000)
-    }
+const rooms = ref([
+  {
+    roomId: '1',
+    roomName: 'Room 1',
+    avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
+    users: [
+      { _id: '1234', username: 'John Doe' },
+      { _id: '4321', username: 'John Snow' }
+    ]
   }
-}
+]);
+const messages = ref([]);
+const messagesLoaded = ref(false);
+
+const fetchMessages = ({ options = {} }) => {
+  setTimeout(() => {
+    if (options.reset) {
+      messages.value = addMessages(true)
+    } else {
+      messages.value = [...addMessages(), ...messages.value]
+      messagesLoaded.value = true
+    }
+  })
+};
+
+const addMessages = (reset: boolean) => {
+  const newMessages = []
+
+  for (let i = 0; i < 30; i++) {
+    newMessages.push({
+      _id: reset ? i : messages.value.length + i,
+      content: `${reset ? '' : 'paginated'} message ${i + 1}`,
+      senderId: '4321',
+      username: 'John Doe',
+      date: '13 November',
+      timestamp: '10:20'
+    })
+  }
+
+  return newMessages
+};
+
+const sendMessage = (message: { content: string }) => {
+  messages.value = [
+    ...messages.value,
+    {
+      _id: messages.value.length,
+      content: message.content,
+      senderId: currentUserId,
+      timestamp: new Date().toString().substring(16, 21),
+      date: new Date().toDateString()
+    }
+  ]
+};
+
+const addNewMessage = () => {
+  setTimeout(() => {
+    messages.value = [
+      ...messages.value,
+      {
+        _id: messages.value.length,
+        content: 'NEW MESSAGE',
+        senderId: '1234',
+        timestamp: new Date().toString().substring(16, 21),
+        date: new Date().toDateString()
+      }
+    ]
+  }, 2000)
+};
 </script>
