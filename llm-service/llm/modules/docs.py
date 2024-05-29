e8db47a491dbe2649effc7d2656a87404e26948c
김상우 <>
2024-05-29 14:56:56 +0900

    [FAB2023-750] fix: docs 모듈 수정 및 urlChain 추가

--- llm-service/llm/modules/docs.py
+++ llm-service/llm/modules/docs.py
@@ -3,6 +3,7 @@ import tempfile
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders import WebBaseLoader
 from llmapp.settings import OPENAI_API_KEY
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -17,8 +18,41 @@ class DoscModule:
         tmp_file_path = cls.read_file(file)
 
         # 문서 로드
-        csvv = CSVLoader(file_path=tmp_file_path)
-        docs = csvv.load()
+        data = CSVLoader(file_path=tmp_file_path)
+        docs = data.load()
+
+        chain = cls.setup_retrieval_pipeline(llm, docs)
+
+        # 임시 파일 삭제
+        if tmp_file_path and os.path.exists(tmp_file_path):
+            os.remove(tmp_file_path)
+
+        return chain
+
+    @classmethod
+    def urlChain(cls, llm, url):
+
+        # 문서 로드
+        data = WebBaseLoader(url)
+        docs = data.load()
+
+        chain = cls.setup_retrieval_pipeline(llm, docs)
+
+        return chain
+
+    @staticmethod
+    def read_file(file):
+        if file.name.endswith('.csv'):
+            # 임시 파일 생성 및 저장
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                for chunk in file.chunks():
+                    tmp_file.write(chunk)
+                tmp_file_path = tmp_file.name
+
+        return tmp_file_path
+
+    @staticmethod
+    def setup_retrieval_pipeline(llm, docs):
 
         # OpenAI 임베딩 생성기 초기화
         embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=OPENAI_API_KEY)
@@ -35,7 +69,7 @@ class DoscModule:
             """
                 기존에 알고 있었던 정보는 모두 잊어.
                 주어진 정보를 참고 하여 답변을 해야해.
-                주어진 정보를 참고 해도 알 수 없을 경우 항상 "답변할 수 없습니다."로 응답 해줘.
+                주어진 정보를 참고 해도 알 수 없을 경우 항상 "제가 가진 정보로는 알 수 없습니다."로 응답 해줘.
                 Context: {context}
                 Question: {input}
             """
@@ -45,19 +79,5 @@ class DoscModule:
         document_chain = create_stuff_documents_chain(llm, prompt)
         chain = create_retrieval_chain(retriever, document_chain)
 
-        # 임시 파일 삭제
-        if tmp_file_path and os.path.exists(tmp_file_path):
-            os.remove(tmp_file_path)
-
         return chain
 
-    @staticmethod
-    def read_file(file):
-        if file.name.endswith('.csv'):
-            # 임시 파일 생성 및 저장
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                for chunk in file.chunks():
-                    tmp_file.write(chunk)
-                tmp_file_path = tmp_file.name
-
-        return tmp_file_path
