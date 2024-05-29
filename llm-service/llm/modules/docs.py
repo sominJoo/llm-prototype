be495f0d546cdbaca19f12fda7daa78e9483b8a8
김상우 <>
2024-05-29 13:25:14 +0900

    [FAB2023-750] fix: docs 모듈 1차 수정

--- llm-service/llm/modules/docs.py
+++ llm-service/llm/modules/docs.py
@@ -1,44 +1,63 @@
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-import pandas as pd
+import os
+import tempfile
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from llmapp.settings import OPENAI_API_KEY
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
 class DoscModule:
     @classmethod
     def docsChain(cls, llm, file):
 
-        # 입력 검증
-        error_response = validate_inputs(file)
-        if error_response:
-            return error_response
+        tmp_file_path = cls.read_file(file)
 
-        # 파일 읽기
-        df, error_response = read_file(file)
-        if error_response:
-            return error_response
+        # 문서 로드
+        csvv = CSVLoader(file_path=tmp_file_path)
+        docs = csvv.load()
 
-        # 에이전트 객체 생성
-        agent = create_pandas_dataframe_agent(llm, df)
+        # OpenAI 임베딩 생성기 초기화
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=OPENAI_API_KEY)
 
-        return agent
+        # 텍스트 분할
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(docs)
 
+        # 벡터스토어에 임베딩
+        vector = FAISS.from_documents(documents, embeddings)
 
-def validate_inputs(file):
-    if not file:
-        return "No file provided"
+        # 프롬프트 지정
+        prompt = ChatPromptTemplate.from_template(
+            """
+                기존에 알고 있었던 정보는 모두 잊어.
+                주어진 정보를 참고 하여 답변을 해야해.
+                주어진 정보를 참고 해도 알 수 없을 경우 항상 "답변할 수 없습니다."로 응답 해줘.
+                Context: {context}
+                Question: {input}
+            """
+        )
 
-    return None
+        retriever = vector.as_retriever()
+        document_chain = create_stuff_documents_chain(llm, prompt)
+        chain = create_retrieval_chain(retriever, document_chain)
 
+        # 임시 파일 삭제
+        if tmp_file_path and os.path.exists(tmp_file_path):
+            os.remove(tmp_file_path)
 
-def read_file(file):
-    if file.name.endswith('.csv'):
-        df = pd.read_csv(file)
-    elif file.name.endswith('.xlsx'):
-        df = pd.read_excel(file)
-    else:
-        return None, "Unsupported file type"
+        return chain
 
-    if df.empty:
-        return None, "The file is empty"
+    @staticmethod
+    def read_file(file):
+        if file.name.endswith('.csv'):
+            # 임시 파일 생성 및 저장
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                for chunk in file.chunks():
+                    tmp_file.write(chunk)
+                tmp_file_path = tmp_file.name
 
-    return df, None
-
+        return tmp_file_path
